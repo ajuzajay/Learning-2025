391d4fc6a10948ea3b9edadb974348a2ce1f07aa
Ajay john <107482384+ajuzajay@users.noreply.github.com>
2025-01-09 11:01:45 +0530

    Update app.py

--- day1/app.py
+++ day1/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is ajay john </h1><br><p>Starting Github action with Akhilesh....</p> '
+    return '<h1> This is Lilly john </h1><br><p>Starting Github action with Akhilesh....</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

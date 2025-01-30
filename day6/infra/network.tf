e986c5a236b72708cd65b4d30fc0e29863aa1feb
ajay john <ajayjohn@ajays-MacBook-Air.local>
2025-01-30 22:25:13 +0530

    adding

--- day6/infra/network.tf
+++ day6/infra/network.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "public_2" {
   tags = {
     Name = "Ecs farget public subnet 2"   
   }
+}
 
 #subnet creation
 
@@ -41,4 +42,47 @@ resource "aws_subnet" "private_1" {
     Name = "Ecs farget private subnet 1"   
   }
 }
+resource "aws_subnet" "private_2" {
+  vpc_id = "${aws_vpc.master.id}"
+  cidr_block = "10.0.4.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = "Ecs farget private subnet 2"   
+  }
+}
+
+#internet gateway creation
+
+resource "aws_internet_gateway" "master" {
+  vpc_id = "${aws_vpc.master.id}"
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+#route table creation
+
+resource "aws_route_table" "public_1" {
+  vpc_id = "${aws_vpc.master.id}"
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = "${aws_internet_gateway.master.id}"
+  }
+  tags = {
+    Name = "Ecs farget public route table 1"
+  }
+}
+
+resource "aws_route_table" "public_2" {
+  vpc_id = "${aws_vpc.master.id}"
+  route {
+    cidr_block = "10.0.2.0/24"
+    gateway_id = "${aws_internet_gateway.master.id}"
+  }
+  tags = {
+    Name = "Ecs farget public route table 2"
+  }
+}
+
+#route table association
 

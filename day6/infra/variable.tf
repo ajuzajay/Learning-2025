349a229a82565e9ebc9054f06afcc8bffaa15997
Ajay John <ajay.john1@ibm.com>
2025-02-10 22:30:08 +0530

    adding

--- day6/infra/variable.tf
+++ day6/infra/variable.tf
@@ -32,3 +32,50 @@ variable "app_name" {
 }
 
 
+variable "db_default_settings" {
+  type = any
+  default = {
+    allocated_storage       = 30
+    max_allocated_storage   = 50
+    engine_version          = 14.15
+    instance_class          = "db.t3.micro"
+    backup_retention_period = 2
+    db_name                 = "postgres"
+    ca_cert_name            = "rds-ca-rsa2048-g1"
+    db_admin_username       = "postgres"
+  }
+}
+
+# variable "flask_app_template_file" {
+#   description = "The path to the template file for the flask app"
+# }
+
+# variable "flask_app_cpu" {
+#   description = "CPU units for the flask-app service"
+#   type        = number
+#   default     = 1024
+# }
+
+# variable "flask_app_memory" {
+#   description = "Memory in MiB for the flask-app service"
+#   type        = number
+#   default     = 2048
+# }
+
+# variable "tag" {
+# }
+
+# variable "desired_flask_task_count" {
+#   description = "The desired number of tasks for the flask-app service"
+#   type        = number
+#   default     = 2
+# }
+
+# variable "public_hosted_zone_id" {
+#   description = "The hosted zone id for the public domain"
+#   default     = "livingdevops.com"
+# }
+
+# variable "domain_name" {
+#   description = "The domain name for the application"
+# }
